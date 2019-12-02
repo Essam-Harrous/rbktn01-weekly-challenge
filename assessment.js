d505370d64aee670437681bc93904a9b0845c081
essam <esamfarg91@gmail.com>
2019-12-02 09:49:21 +0100

    not bad

--- assessment.js
+++ assessment.js
@@ -43,27 +43,27 @@ function daBears() {
 // Which function(s) access the "chair" variable and get "Too Big!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale1 = ["", "papaBear", "mamaBear", "babyBear", ""];
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale2 = ["", "", "", "", "goldilocks"];
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale3 = ["", "", "mamaBear", "babyBear", ""];
 
 // Which function(s) access the "sleepy" variable and get undefined
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale4 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale4 = ["daBears", "papaBear", "mamaBear", "", "goldilocks"];
 
 // Which function(s) access the isFurry variable and get true
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", ""];
 
 
 // *************
@@ -82,6 +82,21 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // "charger" once, and invoke it twice on "mustang".
 
 // CODE HERE...
+class Vehicle {
+    constructor() {
+        this.gasRemaining = 100;
+    }
+    drive() {
+        this.gasRemaining -= 25;
+    }
+}
+
+var charger = new Vehicle();
+var mustang = new Vehicle();
+
+charger.drive();
+mustang.drive();
+mustang.drive();
 
 
 
@@ -103,7 +118,15 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 // Your method may be passed punctuation, numbers or other non-letter characters
 // and should neither modify them nor break when encountering them.
-
+String.prototype.grammarPolice = function() {
+    var arr = this.split(' ');
+    for (var i = 0; i < arr.length; i++) {
+        
+        arr[i] = arr[i].toLowerCase();
+        arr[i][0].toUpperCase();
+    }
+    return arr.join(' ')
+}
 
 
 
@@ -126,6 +149,11 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // In all other cases, return "Different values"
 
 // CODE HERE...
+function valueType(arg1, arg2){
+    if(arg1 === arg2) return 'Exactly the same';
+    if(arg1 == arg2) return 'Same value, different types';
+    return 'Different values'
+}
 
 // *************
 // * PROBLEM 5 *
@@ -152,6 +180,7 @@ function large() {
     return 'My name is ' + this.name + ' and I am very heavy!'
 }
   // CODE HERE...
+  large.call(elephant)
 
 // *************
 // * PROBLEM 6 *
@@ -164,6 +193,9 @@ function large() {
 // and return the bound function.
 
 // CODE HERE...
+function deathStar(capacity, crew) {
+    return capacity.call(crew)
+}
 
 
 // *************
@@ -178,3 +210,8 @@ function large() {
 // The closure function will return the combined value of assets and liabilities.
 
 // CODE HERE...
+function accountingOffice(assets){
+    return function(liabilities) {
+        return assets + liabilities;
+    }
+}
